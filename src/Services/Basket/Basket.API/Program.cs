786b5f1fa42e2ee289f1ea856f75b88648a88292
a-salehi <amirhossein.salehi@hotmail.com>
2022-04-12 02:39:36 +0430

    basket microservices developed

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,35 @@
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Redis Configuration
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+// General Configuration
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
