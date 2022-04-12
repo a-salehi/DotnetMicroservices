57323a0ec61bce68cd1ac881465f390da2f388c4
a-salehi <amirhossein.salehi@hotmail.com>
2022-04-13 03:48:48 +0430

    updated discount api

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,7 +1,11 @@
+using Discount.API.Repositories;
+using Discount.API.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
