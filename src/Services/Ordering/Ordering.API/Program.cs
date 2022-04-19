ae5bdde67f66ceaf1268cc2dd5f786714dcb7210
a-salehi <amirhossein.salehi@hotmail.com>
2022-04-19 21:27:26 +0430

    consumed RabbitMQ event from ordering microservice

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,3 +1,6 @@
+using EventBus.Messages.Common;
+using MassTransit;
+using Ordering.API.EventBusConsumer;
 using Ordering.API.Extensions;
 using Ordering.Application;
 using Ordering.Infrastructure;
@@ -6,6 +9,28 @@ using Ordering.Infrastructure.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// MassTransit-RabbitMQ Configuration
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<BasketCheckoutConsumer>();
+
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+        
+        cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+        {
+            c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+        });
+    });
+});
+
+builder.Services.AddMassTransitHostedService();
+
+// General Configuration
+builder.Services.AddScoped<BasketCheckoutConsumer>();
+builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddControllers();
 
