9198d37b36e346d131b14efe76139776a1ac539b
a-salehi <amirhossein.salehi@hotmail.com>
2022-04-24 19:52:25 +0430

    added LoggingDelegatingHandler for intercepting microservices request-response

--- src/WebApps/AspnetBasics/Program.cs
+++ src/WebApps/AspnetBasics/Program.cs
@@ -6,14 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog(SeriLogger.Configure);
 
+builder.Services.AddTransient<LoggingDelegatingHandler>();
+
 builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
-                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
+                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>();
 
 builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
-    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
+                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>();
 
 builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
-    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
+                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>();
 
 builder.Services.AddRazorPages();
 
