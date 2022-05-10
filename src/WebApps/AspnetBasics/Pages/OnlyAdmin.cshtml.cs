9e4bc93a1af8d9ef6e4441c27eccd78e3dce1e96
a-salehi <amirhossein.salehi@hotmail.com>
2022-05-10 23:11:28 +0430

    updated

--- src/WebApps/AspnetBasics/Pages/OnlyAdmin.cshtml.cs
+++ src/WebApps/AspnetBasics/Pages/OnlyAdmin.cshtml.cs
@@ -6,18 +6,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspnetBasics.Pages
 {
+    [Authorize(Roles = "admin")]
     public class OnlyAdminModel : PageModel
     {
         private readonly IUserService _userService;
 
-        [Authorize(Roles = "admin")]
+        public OnlyAdminModel(IUserService userService)
+        {
+            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+        }
+        public UserInfoViewModel UserInfo { get; set; } = new UserInfoViewModel(null);
+
         public async Task<IActionResult> OnGetAsync()
         {
             UserInfo = await _userService.GetUserInfo();
 
             return Page();
-        }
-
-        public UserInfoViewModel UserInfo { get; set; } = new UserInfoViewModel(null);
+        }        
     }
 }
